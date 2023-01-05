c1066e7abb5f2ef3df8d059baa93a3135f2fd319
c-guill <guilliercamille@hotmail.com>
2023-01-05 11:37:53 +0100

    modif etape 4.h

--- getELF.h
+++ getELF.h
@@ -5,7 +5,7 @@
 Elf32_Ehdr lireHeaderElf(FILE* f);
 Elf32_Shdr lireSectionHeader(FILE* f);
 Elf32_Shdr *lireSecHeaTable(FILE* f, Elf32_Ehdr ehdr);
-Elf32_Sym *lireSymTable(FILE* f, Elf32_Ehdr shdr);
+Elf32_Sym *lireSymTable(FILE* f, Elf32_Shdr shdr);
 Elf32_Sym lireSymbol(FILE* f);
 Elf32_Rel lireRelocation(FILE* f);
 Elf32_Rela lireRelocationA(FILE* f);
