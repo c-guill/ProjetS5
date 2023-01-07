56d7db94736d3d04ff50ad5b21363f7b822f52c4
LuKP17 <lucaspottier38@gmail.com>
2023-01-07 17:14:46 +0100

    Update getELF.h

--- getELF.h
+++ getELF.h
@@ -4,15 +4,15 @@
 #include <stdio.h>
 
 typedef struct {
-	Elf32_Ehdr ehdr;			// en-tete ELF
-	Elf32_Shdr *shdrtab;		// table d'en-tetes de section
-	char *shstrtab;				// table de chaines d'en-tete de section
-	char **scodetab;			// table des contenus des sections de code
-	Elf32_Sym *symtab;			// table des symboles
-	char *strtab;				// table de chaines
-	Elf32_Rel **reltabtab;		// table des tables de reimplantations
-	Elf32_Rela **relatabtab;	// table des tables de reimplantations addend
-	char *arm_attrib;			// contenu de la section .ARM.attributes
+    Elf32_Ehdr ehdr;            // en-tete ELF
+    Elf32_Shdr *shdrtab;        // table d'en-tetes de section
+    char *shstrtab;             // table de chaines d'en-tete de section
+    char **scodetab;            // table des contenus des sections de code
+    Elf32_Sym *symtab;          // table des symboles
+    char *strtab;               // table de chaines
+    Elf32_Rel **reltabtab;      // table des tables de reimplantations
+    Elf32_Rela **relatabtab;    // table des tables de reimplantations addend
+    char *arm_attrib;           // contenu de la section .ARM.attributes
 } ELF_FILE;
 
 
